26be47b8656d2eef852c71efb1551eb5b0954257
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2023-08-04 00:43:08 +0530

    commit 2
    this commit is done to address a new heading and adding props to camera

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ import { Carousel } from "react-responsive-carousel";
 import ThreeScene from "./Components/ThreeScene";
 import SlideCard from "./Components/SlideCard"; // Import the SlideCard component
 import "./App.css";
+// import "https://fonts.googleapis.com/css?family=Varela" ;
+import "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" ;
+
 
 
 class App extends Component {
   render() {
     return (
       <div id="body">
-        <h1 text-align="centre">Kuch kuch likhwa diya hai chatgpt se</h1>
+        <h1 text-align="centre">
+
+<div class="glitch" data-text="GLITCH">GLITCH</div>
+        
+        </h1>
+        <br/><br/><br/><br/><br/><br/>
         <div className="container carousel-wrapper">
           <Carousel autoPlay={true} showThumbs={false} infiniteLoop={true}>
             <div>
-              <div id="3d">
-                <ThreeScene modelPath="model1/scene.gltf" />
-              </div>
+              <ThreeScene modelPath="model1/scene.gltf" camx={0} camy={1} camz={5}  />
               <SlideCard
                 title="Slide Card 1"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <div id="3d">
-                <ThreeScene modelPath="model4/scene.gltf" />
-              </div>
+              <ThreeScene modelPath="model4/scene.gltf" camx={0} camy={1} camz={3} />
               <SlideCard
                 title="Slide Card 2"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <div id="3d">
-                <ThreeScene modelPath="model5/scene.gltf" />
-              </div>
+              <ThreeScene modelPath="model5/scene.gltf" camx={0} camy={1} camz={1} />
               <SlideCard
                 title="Slide Card 3"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <div id="3d">
-                <ThreeScene modelPath="model/scene.gltf" />
-              </div>
+              <ThreeScene modelPath="model/scene.gltf" camx={0} camy={1} camz={3} />
               <SlideCard
                 title="Slide Card 4"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
-            {/* Add more slides with different 3D models */}
+            
           </Carousel>
         </div>
       </div>
--- src/Components/ThreeScene.js
+++ src/Components/ThreeScene.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import TWEEN from 'tween.js';
 
 class ThreeScene extends Component {
   componentDidMount() {
@@ -9,68 +10,117 @@ class ThreeScene extends Component {
   }
 
   initThree() {
-  const { modelPath } = this.props;
-  const scene = new THREE.Scene();
-  scene.background = null; // Set scene background to transparent
+    // const container = document.getElementById('slider-container');
+    const { modelPath ,camx,camy,camz} = this.props;
+    const scene = new THREE.Scene();
+    scene.background = null; // Set scene background to transparent // has the potential to be rendered as loader // obs 1 whilst reading the code base
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-  const renderer = new THREE.WebGLRenderer({ alpha: true }); // Enable alpha for transparent background
-  renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
-  renderer.setClearColor(0x000000, 0); // Set clearColor with alpha value of 0
-  this.threeContainer.appendChild(renderer.domElement);
+    const renderer = new THREE.WebGLRenderer({ alpha: true }); // Enable alpha for transparent background
+    renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+    renderer.setClearColor(0x000000, 0); // Set clearColor with alpha value of 0
+    this.threeContainer.appendChild(renderer.domElement);
 
-  // Load the 3D model using GLTFLoader
-  const loader = new GLTFLoader();
-  loader.load(
-    modelPath,
-    (gltf) => {
-      const model = gltf.scene;
-      // Traverse through the model and set material to transparent
-      model.traverse((child) => {
-        if (child.isMesh) {
-          child.material.transparent = true;
-        }
-      });
-      scene.add(model);
-    },
-    undefined,
-    (error) => {
-      console.error("Error loading 3D model", error);
-    }
-  );
+    // Load the 3D model using GLTFLoader
+    const loader = new GLTFLoader();
+    loader.load(
+      modelPath,
+      (gltf) => {
+        const model = gltf.scene;
+        // Traverse through the model and set material to transparent
+        model.traverse((child) => {
+          if (child.isMesh) {
+            child.material.transparent = true;
+          }
+        });
+        scene.add(model);
+      },
+      undefined,
+      (error) => {
+        console.error("Error loading 3D model", error);
+      }
+    );
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  // Set camera position
-  camera.position.set(0, 1, 3);
-  scene.rotation.y = 7 * Math.PI / 4;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    
+    // to disable zoom
+controls.enableZoom = false;
 
-  controls.update();
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-  scene.add(directionalLight);
+// to disable rotation
+// controls.enableRotate = false;
 
-  let rotationAngle = 0;
+// to disable pan
+controls.enablePan = false;
+// controls.minPolarAngle = Math.PI/2;
+// controls.maxPolarAngle = Math.PI/2;
+    // Set camera position
+    camera.position.set(camx, camy, camz);
+    scene.rotation.y = 7 * Math.PI / 4;
 
-  // Animation loop
-  const animate = () => {
-    requestAnimationFrame(animate);
-    rotationAngle += 0.01;
-    scene.rotation.y = rotationAngle;
     controls.update();
 
-    // You can add any animation or updates to the 3D model here
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    scene.add(directionalLight);
 
-    renderer.render(scene, camera);
-  };
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-  animate();
-}
+    let rotationAngle = 0;
 
+    // Animation loop
+    // function animateRotation() {
+    //   // Rotate the loader smoothly using Tween.js
+    //   const rotationDuration = 4; // Duration of rotation in seconds
+    //   const targetRotationX = loader.rotation.x + Math.PI * 2; // Rotate 360 degrees
+    //   const targetRotationY = loader.rotation.y + Math.PI * 2; // Rotate 360 degrees
+
+    //   // Create the rotation tweens for X and Y axes
+    //   const tweenX = new TWEEN.Tween(loader.rotation)
+    //     .to({ x: targetRotationX }, rotationDuration * 1000)
+    //     .easing(TWEEN.Easing.Linear.None);
+
+    //   const tweenY = new TWEEN.Tween(loader.rotation)
+    //     .to({ y: targetRotationY }, rotationDuration * 1000)
+    //     .easing(TWEEN.Easing.Linear.None);
+
+    //   // Chain the tweens for smooth rotation on both axes
+    //   tweenX.chain(tweenY);
+
+    //   // Start the rotation
+    //   tweenX.start();
+    // }
+    window.addEventListener('resize', () => {
+      // update display width and height
+      var width = window.innerWidth
+     var height = window.innerHeight
+      // update camera aspect
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+      // update renderer
+      renderer.setSize(width, height)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+      renderer.render(scene, camera)
+   })
+    const animate = () => {
+      requestAnimationFrame(animate);
+      rotationAngle += 0.01;
+      scene.rotation.y = rotationAngle;
+      controls.update();
+
+      // You can add any animation or updates to the 3D model here
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+    // animateRotation();
+  }
 
   componentWillUnmount() {
     // Perform any cleanup here, e.g., remove event listeners, dispose of objects

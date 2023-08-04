77792f5d674af3a7cb22d8fc91ba7c12c8ddef18
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2023-08-05 00:27:40 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Carousel } from "react-responsive-carousel";
 import ThreeScene from "./Components/ThreeScene";
 import SlideCard from "./Components/SlideCard"; // Import the SlideCard component
 import "./App.css";
-// import "https://fonts.googleapis.com/css?family=Varela" ;
+import '@fontsource/eczar' ;
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" ;
 
 
@@ -13,13 +13,14 @@ import "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" ;
 class App extends Component {
   render() {
     return (
+      <>
     <div id="bg">
     <div id='stars'></div>
 <div id='stars2'></div>
 <div id='stars3'></div>    
       <div id="body">
         <h1 text-align="centre">
-<div background-color= "transparent" class="glitch" data-text="GLITCH">COPS</div>
+<div background-color= "transparent" class="glitch" data-text="CODING">COPS</div>
         </h1>
         <br/>
         <div className="container carousel-wrapper">
@@ -28,35 +29,69 @@ class App extends Component {
               <ThreeScene modelPath="model1/scene.gltf" camx={0} camy={5} camz={50}  />
               <SlideCard
                 title="Competitive Programming"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                text="Get yourselves competing for the best ranks amongst the coders that have their ratings sky-rocketing."
               />
             </div>
             <div>
               <ThreeScene modelPath="brain/scene.gltf" camx={3.5} camy={5} camz={0} />
               <SlideCard
                 title="Intelligence Group"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                text="Some of the best brains WORTHY enough to train the machines woke up and chose violence . "
               />
             </div>
             <div>
               <ThreeScene modelPath="react/scene.gltf" camx={0} camy={0} camz={4} />
               <SlideCard
                 title="Software Development Guild"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                text="You talk as we code , The most creative peeps meet up to make bring ideas to life ."
               />
             </div>
             <div>
-              <ThreeScene modelPath="InfoSec/scene.gltf" camx={5} camy={1} camz={1} />
+              <ThreeScene modelPath="InfoSec/scene.gltf" camx={0} camy={0.1} camz={0.2} />
               <SlideCard
-                title="Information Security"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
+                title="InfoSec"
+                text="The coolest profile one can dream of . Hacking , Phishing , Jailbreaks ... They got you covered! "
               />
             </div>
             
           </Carousel>
         </div>
+
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       </div>
+      <br/>
+        <p id="content" style={{ fontFamily: 'Eczar, serif'}}>
+        In this age of increasing automation, programming is not limited to only computer science and related fields. Every competent engineer needs to know how to program.
+        <br/>
+        <br/>
+         This makes Club of Programmers one of the most important clubs in the institute.
+
+Club of Programmers (or better known as COPS) understands this responsibility and plays a crucial part in maintaining the coding culture in the institute. 
+Introductory workshops are held for freshers to introduce them to the various facets of programming. Regular competitions are held for the general student body to test and sharpen their programming skills. 
+        </p>
+      <br/>
+      <h1 id="hiya" style={{ fontFamily: 'Eczar, serif'}}>How to become a COP ?</h1>
+      <br/>
+
+      <ul style={{ fontFamily: 'Eczar, serif'}}>
+        <li  >Interest and consistent effort are the only pre-requisites :)</li>
+        <li >Attend workshops</li>
+
+        <li >Stalk your seniors :P</li>
+
+        <li >Ask for help</li>
+
+        <li >Follow our Instagram for updates</li>
+
+        <li >Join Discord for communication</li>
+
+        <li >Try your hand at our events</li>
+      </ul>
         </div>
+        </>
     );
   }
 }
--- src/Components/SlideCard.js
+++ src/Components/SlideCard.js
@@ -7,7 +7,9 @@ const SlideCard = ({ title, text }) => {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title" style={{ fontFamily: 'Eczar, serif'}}>{title}</h5>
+        <br/>
         <p className="card-text" >{text}</p>
+        <br/>
       </div>
     </div>
   );

a4014731f6967f2cc7f784062bedcf8898f31b3a
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2023-08-04 15:34:15 +0530

    Revert "test"
    This reverts commit e49c9a7d78075ea7b8200983b3cfbd3f9ad79156.

--- src/App.js
+++ src/App.js
@@ -14,12 +14,14 @@ class App extends Component {
   render() {
     return (
       <div id="body">
-
-
         <h1 text-align="centre">
+<<<<<<< Updated upstream
+<div background-color= "transparent" class="glitch" data-text="GLITCH">COPS</div>
+=======
 
 <div background-color= "transparent" class="glitch" data-text="COPS">COPS</div>
         
+>>>>>>> Stashed changes
         </h1>
         <br/>
         <div className="container carousel-wrapper">
@@ -27,34 +29,33 @@ class App extends Component {
             <div>
               <ThreeScene modelPath="model1/scene.gltf" camx={0} camy={5} camz={50}  />
               <SlideCard
-                title="Slide Card 1"
+                title="Competitive Programming"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <ThreeScene modelPath="model4/scene.gltf" camx={0} camy={100} camz={300} />
+              <ThreeScene modelPath="brain/scene.gltf" camx={3.5} camy={5} camz={0} />
               <SlideCard
-                title="Slide Card 2"
+                title="Intelligence Group"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <ThreeScene modelPath="model5/scene.gltf" camx={0} camy={2} camz={3} />
+              <ThreeScene modelPath="react/scene.gltf" camx={0} camy={0} camz={4} />
               <SlideCard
-                title="Slide Card 3"
+                title="Software Development Guild"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             <div>
-              <ThreeScene modelPath="model/scene.gltf" camx={0} camy={1} camz={10} />
+              <ThreeScene modelPath="InfoSec/scene.gltf" camx={5} camy={1} camz={1} />
               <SlideCard
-                title="Slide Card 4"
+                title="Information Security"
                 text="Some quick example text to build on the card title and make up the bulk of the card's content."
               />
             </div>
             
           </Carousel>
-        </div>
         </div>
       </div>
     );
